194c91f59618abee4bfce2fee6110c7bc93ff9fc
JoelCCK12 <joel.cruz.intern@ck12.org>
2025-08-25 10:07:19 -0700

    initialized stellar fusion interactive

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import StellarFusion from './components/StellarFusion'
+
+
+function App() {
+  return (
+      <div className="App">
+        <StellarFusion />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/StellarFusion.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const StellarFusion = () => {
+
+
+	return (
+        <div>test</div>
+)
+};
+
+
+export default StellarFusion;
